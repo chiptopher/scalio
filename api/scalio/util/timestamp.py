c9655642efb43b9898bbe8751ecb14030b82e1d2
chiptopher <chrisboyerdev@gmail.com>
2018-11-23 00:27:27 -0500

    Change date to timestamp conversion method

--- api/scalio/util/timestamp.py
+++ api/scalio/util/timestamp.py
@@ -3,7 +3,7 @@ import datetime
 
 
 def convert_date_to_timestamp(date: str):
-    return time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple()) * 1000
+    return round(datetime.datetime.strptime(date, "%Y-%m-%d").timestamp()*1000)
 
 
 def time_in_millis(time_to_convert: datetime.datetime = datetime.datetime.utcnow(),
