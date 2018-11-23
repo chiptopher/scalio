7efa3ca9f84267845a551e425ab5ccd1dc236535
chiptopher <chrisboyerdev@gmail.com>
2018-11-23 00:44:29 -0500

    Appropriately handle difference in timezone

--- api/scalio/util/timestamp.py
+++ api/scalio/util/timestamp.py
@@ -1,9 +1,12 @@
 import time
 import datetime
+from pytz import timezone
 
 
 def convert_date_to_timestamp(date: str):
-    return round(datetime.datetime.strptime(date, "%Y-%m-%d").timestamp()*1000)
+    return round(datetime.datetime.strptime(date, "%Y-%m-%d")
+                 .replace(tzinfo=timezone('UTC'))
+                 .timestamp()*1000)
 
 
 def time_in_millis(time_to_convert: datetime.datetime = datetime.datetime.utcnow(),
--- api/test/util/test_timestamp.py
+++ api/test/util/test_timestamp.py
@@ -5,4 +5,4 @@ from scalio.util import timestamp
 class TimestampTest(unittest.TestCase):
 
     def test_convert_date_to_timestamp_covnerts_yyyy_mm_dd_to_millis_from_epoch(self):
-        self.assertEqual(1514782800000, timestamp.convert_date_to_timestamp('2018-01-01'))
+        self.assertEqual(1514764800000, timestamp.convert_date_to_timestamp('2018-01-01'))
